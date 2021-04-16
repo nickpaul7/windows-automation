fab47cd378fa26ff114e6bd1531efb3a2a3f89c9
nickpaul7 <nickryanpaul@example.com>
2021-04-16 16:44:24 -0400

    added process to build conda enviroment, install packages, and launch bokeh server.

--- run_bokeh_server.bat
+++ run_bokeh_server.bat
@@ -1,2 +1,4 @@
-call activate bokeh_server
+call activate bokeh_app
 bokeh serve --show c:\Users\nickr\Documents\Projects\bokeh\examples\app\sliders.py
+call deactivate bokeh_app
+PAUSE
--- setup_bokeh_env.bat
+++ setup_bokeh_env.bat
@@ -1,6 +1,6 @@
 echo y| conda create -n "bokeh_app"
 call activate bokeh_app
-echo y | conda install bokeh pandas numpy
+echo y | conda install tornado bokeh pandas numpy
 call run_bokeh_server.bat
 PAUSE
 
